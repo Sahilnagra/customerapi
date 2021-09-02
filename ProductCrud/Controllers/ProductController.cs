f8d5de39e340b5ce1e4e33e2addba0eebe284504
Sahilnagra <52886762+Sahilnagra@users.noreply.github.com>
2021-09-03 00:21:39 +0530

    Add files via upload

---
+++ ProductCrud/Controllers/ProductController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProductCrud.Models;
+using ProductCrud.ProductData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCrud.Controllers
+{
+    [ApiController]
+    
+    public class ProductController : ControllerBase
+    {
+        private IProductData _productData;
+      public ProductController(IProductData productData)
+        {
+            _productData = productData;
+        }
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult GetProduct()
+        {
+            return Ok(_productData.GetProduct());
+        }
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public IActionResult GetProduct(Guid id)
+        {
+            var product = _productData.GetProduct(id);
+            if (product != null)
+            {
+                return Ok(_productData.GetProduct(id));
+            }
+            return NotFound($"Product with Id: {id} was not found");
+
+        }
+        [HttpPost]
+        [Route("api/[controller]")]
+        public IActionResult GetProduct(Product product)
+        {
+             _productData.AddProduct(product);
+            return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path +
+                "/" + product.Id, product);
+
+        }
+
+
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+        public IActionResult DeleteProduct(Guid id)
+        {
+            var product = _productData.GetProduct(id);
+            if(product != null)
+            {
+                _productData.DeleteProduct(product);
+                return Ok();
+            }
+            return NotFound($"Product with Id: {id} was not found");
+
+        }
+        [HttpPatch]
+        [Route("api/[controller]/{id}")]
+        public IActionResult EditProduct(Guid id, Product product)
+        {
+
+            var existingproduct = _productData.GetProduct(id);
+            if (existingproduct != null)
+            {
+                product.Id = existingproduct.Id;
+                _productData.EditProduct(product);
+                
+            }
+            return Ok(product);
+        }
+
+
+
+
+    }
+    }
+
---
+++ ProductCrud/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCrud.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage ="Name can only be of 20 characters")]
+        public string Name { get; set; }
+
+       
+    }
+}
